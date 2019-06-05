32604518b7d65028ddf56fc8515dbe3be6c3e05e
Tommaso Bertoni <bertonitommaso@gmail.com>
2019-06-06 00:30:31 +0200

    Added ParametersSetCollectionTests.

---
+++ tests/TinyBenchmark.Analysis.Tests/Parameters/ParametersSetCollectionTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TinyBenchmark.Analysis;
+
+namespace Analysis.Parameters
+{
+    [TestFixture]
+    public class ParametersSetCollectionTests
+    {
+        [Test]
+        public void CollectionCanBeEmpty()
+        {
+            var collection = new ParametersSetCollection();
+            Assert.That(collection.ParametrizedPropertiesCount, Is.EqualTo(0));
+
+            var parametersSets = collection.ToList();
+            Assert.That(parametersSets, Is.Not.Null);
+            Assert.That(parametersSets, Is.Empty);
+        }
+    }
+}
